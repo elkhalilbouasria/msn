bea9fc5291ef0995b5810f3f06cc56762a84c295
ElKhalilBouasria <elkhalilbouasria.dev@gmail.com>
2023-11-13 17:47:48 +0100

    test

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,14 +1,14 @@
 export const AppsData = [
     {
         name:"MSN",
-        img: "public/assets/images/msnIcon.png"
+        img: "src/assets/images/msnIcon.png"
     },
     {
         name:"My photos",
-        img: "public/assets/images/folderIcon.png"
+        img: "src/assets/images/folderIcon.png"
     },
     {
         name:"Music Playlist",
-        img:"public/assets/images/playlistIcon.png"
+        img:"src/assets/images/playlistIcon.png"
     }
 ]
